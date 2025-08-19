57b2a331446d1a4a3a889c9f372f759e533a7e2a
Alberto3375 <127156968+Alberto3375@users.noreply.github.com>
2025-08-19 13:10:54 -0600

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -114,7 +114,6 @@ const Hero = () => {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              Nuestra historia
             </motion.button>
           </motion.div>
 
